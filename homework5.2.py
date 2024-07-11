b3f3dc0d58f45f085d13dcbaaae8e69b5d6bc55d
Danil22134 <kuchinskiy-danil7@mail.ru>
2024-07-11 23:23:12 +0700

    Update homework5.2.py

--- homework5.2.py
+++ homework5.2.py
@@ -4,6 +4,12 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: "{self.name}", кол-во этажей: {self.number_of_floors}.'
+
     def go_to(self, new_floor):
         if 0 < new_floor <= self.number_of_floors:
             for floor in range(1, new_floor + 1):
@@ -16,15 +22,6 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
-
-
-def __len__(self):
-    return self.number_of_floors
-
-
-def __str__(self):
-    return "Название: <название>, кол-во этажей: <этажи>"
-
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
